6b93e992a38afad4c8e8e4c975f44c0aa47462e2
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-11-18 07:26:15 +0000

    pages controller作成

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(params_pages)
+    @page.save
+    render :action 'new'
+  end
+  
+  private
+  
+  def params_pages
+    params.require(:page).permit(:title,:content,:email)
+    
+end
+end
